db5879a6a84bc430acb1c07e7eb1e9937ba7016f
irbude <46744238+irbude@users.noreply.github.com>
2019-03-07 21:43:47 +0100

    Add files via upload

--- three_sensor_ros.ino
+++ three_sensor_ros.ino
@@ -1,6 +1,5 @@
 /*
   Maxbotix simple test
-
   Instructions:
   - At least one of: (comment the appropriate code below)
       PW is digital pin 8
@@ -8,7 +7,6 @@
       AN is analog pin A0
   - Change code below according to your model (LV, XL and
   HRLV supported)
-
   Note:
   For convenience, the getRange method will always return centimeters.
   You can use convert fuctions to convert to another unit (toInches and
@@ -20,17 +18,16 @@
 #include <sensor_msgs/Range.h>
 #include "Maxbotix.h"
 
-ros::NodeHandle  nhL;
+ros::NodeHandle  nh;
+
 sensor_msgs::Range range_msgL;
 ros::Publisher pub_rangeL( "/ultrasoundL", &range_msgL);
 char frameidL[] = "/ultrasoundL";
 
-ros::NodeHandle  nhR;
 sensor_msgs::Range range_msgR;
 ros::Publisher pub_rangeR( "/ultrasoundR", &range_msgR);
 char frameidR[] = "/ultrasoundR";
 
-ros::NodeHandle  nhC;
 sensor_msgs::Range range_msgC;
 ros::Publisher pub_rangeC( "/ultrasoundC", &range_msgC);
 char frameidC[] = "/ultrasoundC";
@@ -50,14 +47,11 @@ long range_time = 0;
 void setup()
 {
   //Initiating nodes
-  nhL.initNode();
-  nhL.advertise(pub_rangeL);
+  nh.initNode();
+  nh.advertise(pub_rangeL);
+  nh.advertise(pub_rangeR);
+  nh.advertise(pub_rangeC);
 
-  nhR.initNode();
-  nhR.advertise(pub_rangeR);
-
-  nhC.initNode();
-  nhC.advertise(pub_rangeC);
 
 
   //Setting up information of range messages
@@ -90,16 +84,14 @@ void loop()
     range_msgR.range = rangeSensorPWR.getRange();
     range_msgC.range = rangeSensorPWC.getRange();
 
-    range_msgL.header.stamp = nhL.now();
-    range_msgR.header.stamp = nhR.now();
-    range_msgC.header.stamp = nhC.now();
+    range_msgL.header.stamp = nh.now();
+    range_msgR.header.stamp = range_msgL.header.stamp;
+    range_msgC.header.stamp = range_msgL.header.stamp;
 
     pub_rangeL.publish(&range_msgL);
     pub_rangeR.publish(&range_msgR);
     pub_rangeC.publish(&range_msgC);
 
   }
-  nhR.spinOnce();
-  nhL.spinOnce();
-  nhC.spinOnce();
+  nh.spinOnce();
 }
